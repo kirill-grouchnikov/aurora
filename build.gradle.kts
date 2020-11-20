dc1b8fba977bedccf5a276492cdab090f7627b9d
Kirill Grouchnikov <kirillg@google.com>
2020-11-20 16:05:30 -0500

    Tweak the build file
    Gradle -> 6.7.1
    Add dependency version tracking

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,12 +2,14 @@ buildscript {
     repositories {
         google()
         jcenter()
+        mavenCentral()
         maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
     }
 
     dependencies {
         classpath("org.jetbrains.compose:compose-gradle-plugin:0.2.0-build127")
         classpath(kotlin("gradle-plugin", version = "1.4.10"))
+        classpath("com.github.ben-manes:gradle-versions-plugin:0.36.0")
     }
 }
 
@@ -19,3 +21,8 @@ allprojects {
         maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
     }
 }
+
+// To generate report about available dependency updates, run
+// ./gradlew dependencyUpdates
+apply(plugin = "com.github.ben-manes.versions")
+
