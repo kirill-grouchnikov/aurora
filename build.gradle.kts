75aac6d300364f81981077e89273445335e5158c
Kirill Grouchnikov <kirillg@google.com>
2021-01-08 17:13:04 -0500

    Preparing for publishing

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,6 +10,8 @@ buildscript {
         classpath("org.jetbrains.compose:compose-gradle-plugin:0.3.0-build139")
         classpath(kotlin("gradle-plugin", version = "1.4.21"))
         classpath("com.github.ben-manes:gradle-versions-plugin:0.36.0")
+        classpath("com.vanniktech:gradle-maven-publish-plugin:0.13.0")
+        classpath("org.jetbrains.dokka:dokka-gradle-plugin:1.4.10.2")
     }
 }
 
@@ -20,7 +22,14 @@ allprojects {
         jcenter()
         maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
     }
+
+    tasks.withType<Jar>().configureEach {
+        isPreserveFileTimestamps = false
+        isReproducibleFileOrder = true
+        archiveBaseName.set("${rootProject.name}-${project.name}")
+    }
 }
+
 
 // To generate report about available dependency updates, run
 // ./gradlew dependencyUpdates
