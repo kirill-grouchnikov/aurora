ae9a17a967925b0cf2a5a5fa11725de518b0fbb0
Kirill Grouchnikov <kirillg@google.com>
2021-01-12 10:00:27 -0500

    Upgrade to build 140

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,11 +7,11 @@ buildscript {
     }
 
     dependencies {
-        classpath("org.jetbrains.compose:compose-gradle-plugin:0.3.0-build139")
+        classpath("org.jetbrains.compose:compose-gradle-plugin:0.3.0-build140")
         classpath(kotlin("gradle-plugin", version = "1.4.21"))
         classpath("com.github.ben-manes:gradle-versions-plugin:0.36.0")
         classpath("com.vanniktech:gradle-maven-publish-plugin:0.13.0")
-        classpath("org.jetbrains.dokka:dokka-gradle-plugin:1.4.10.2")
+        classpath("org.jetbrains.dokka:dokka-gradle-plugin:1.4.20")
     }
 }
 
