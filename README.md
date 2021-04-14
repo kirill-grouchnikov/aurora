1a768440122d216bb03d1c51603ae00b0638f299
Kirill Grouchnikov <kirill.grouchnikov@gmail.com>
2021-04-14 09:17:19 -0400

    Bump to 0.0.24
    And remove all jcenter / bintray dependencies

--- README.md
+++ README.md
@@ -22,8 +22,8 @@ Aurora snapshot builds are made available on Sonatype.
 Add the latest Kotlin and Compose Desktop dependencies:
 ```kotlin
 plugins {
-    kotlin("jvm") version "1.4.31"
-    id("org.jetbrains.compose") version "0.4.0-build178"
+    kotlin("jvm") version "1.4.32"
+    id("org.jetbrains.compose") version "0.4.0-build182"
 }
 ```
 
@@ -36,10 +36,10 @@ Add Aurora dependencies:
 
 ```
 dependencies {
-    implementation("org.pushing-pixels:aurora-skin:0.0.23-SNAPSHOT")
-    implementation("org.pushing-pixels:aurora-icon-icon:0.0.23-SNAPSHOT")
-    implementation("org.pushing-pixels:aurora-component:0.0.23-SNAPSHOT")
-    implementation("org.pushing-pixels:aurora-window:0.0.23-SNAPSHOT")
+    implementation("org.pushing-pixels:aurora-skin:0.0.24-SNAPSHOT")
+    implementation("org.pushing-pixels:aurora-icon-icon:0.0.24-SNAPSHOT")
+    implementation("org.pushing-pixels:aurora-component:0.0.24-SNAPSHOT")
+    implementation("org.pushing-pixels:aurora-window:0.0.24-SNAPSHOT")
     implementation(compose.desktop.currentOs)
 }
 ```
--- build.gradle.kts
+++ build.gradle.kts
@@ -5,6 +5,7 @@ buildscript {
         maven("https://plugins.gradle.org/m2/")
         maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
         maven("https://maven.pkg.jetbrains.space/kotlin/p/dokka/dev")
+        maven("https://maven.pkg.jetbrains.space/public/p/kotlinx-html/maven")
     }
 
     dependencies {
@@ -12,7 +13,7 @@ buildscript {
         classpath(kotlin("gradle-plugin", version = "1.4.32"))
         classpath("com.github.ben-manes:gradle-versions-plugin:0.38.0")
         classpath("com.vanniktech:gradle-maven-publish-plugin:0.14.2")
-        classpath("org.jetbrains.dokka:dokka-gradle-plugin:1.4.20")
+        classpath("org.jetbrains.dokka:dokka-gradle-plugin:1.4.32-dev-92")
     }
 }
 
@@ -22,6 +23,7 @@ allprojects {
         mavenCentral()
         maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
         maven("https://maven.pkg.jetbrains.space/kotlin/p/dokka/dev")
+        maven("https://maven.pkg.jetbrains.space/public/p/kotlinx-html/maven")
     }
 
     tasks.withType<Jar>().configureEach {
