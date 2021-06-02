d89cb44c6eab3a895a3d6d0098e3b6c80687c398
Kirill Grouchnikov <kirillg@google.com>
2021-06-03 01:22:35 +0300

    Wire selection change to revealing the new selection
    Hoist `LazyListState` to top level (with the new experimental `application` composable), along with a coroutine scope.
    Add a `LazyListState` extension function to check if a specific item is fully visible.
    When selection changes with up/down arrows, use `LazyListState.scrollToItem` to make the newly selected item fully visible

--- demo/src/desktopMain/kotlin/org/pushingpixels/aurora/demo/AuroraScrollDemo.kt
+++ demo/src/desktopMain/kotlin/org/pushingpixels/aurora/demo/AuroraScrollDemo.kt
@@ -17,18 +17,23 @@ package org.pushingpixels.aurora.demo
 
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyListState
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.foundation.rememberScrollbarAdapter
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.input.key.Key
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.IntSize
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.window.application
+import kotlinx.coroutines.launch
 import org.pushingpixels.aurora.AuroraSkin
 import org.pushingpixels.aurora.IconFilterStrategy
 import org.pushingpixels.aurora.Side
@@ -44,122 +49,158 @@ import org.pushingpixels.aurora.component.projection.ComboBoxProjection
 import org.pushingpixels.aurora.component.projection.LabelProjection
 import org.pushingpixels.aurora.demo.svg.material.*
 import org.pushingpixels.aurora.skin.getAuroraSkins
-import org.pushingpixels.aurora.skin.graphiteAquaSkin
+import org.pushingpixels.aurora.skin.twilightSkin
 import org.pushingpixels.aurora.window.AuroraWindow
 
+fun LazyListState.isItemFullyVisible(index: Int) : Boolean {
+    val layoutInfo = this.layoutInfo
+    val visibleItemsInfo = layoutInfo.visibleItemsInfo
+
+    // Is it in visible items?
+    val visibleItemInfo = visibleItemsInfo.find { it.index == index } ?: return false
+
+    val visibleTop = visibleItemInfo.offset
+    val visibleSize = visibleItemInfo.size
+    val viewportTop = layoutInfo.viewportStartOffset
+    val viewportBottom = layoutInfo.viewportEndOffset
+
+    if (visibleTop < viewportTop) {
+        return false
+    }
+    if ((visibleTop + visibleSize) > viewportBottom) {
+        return false
+    }
+
+    return true
+}
+
+@ExperimentalComposeUiApi
 fun main() {
-    val icons = arrayOf(
-        accessibility_new_24px.factory(),
-        account_box_24px.factory(),
-        backup_24px.factory(),
-        brightness_medium_24px.factory(),
-        help_24px.factory(),
-        info_24px.factory(),
-        keyboard_capslock_24px.factory(),
-        location_on_24px.factory(),
-        perm_device_information_24px.factory(),
-        storage_24px.factory(),
-        visibility_24px.factory(),
-        waves_24px.factory()
-    )
-    val currentAuroraSkin = mutableStateOf(graphiteAquaSkin())
-    val stateSelection = mutableStateOf(-1)
-    val itemCount = 30
+    application {
+        val icons = arrayOf(
+            accessibility_new_24px.factory(),
+            account_box_24px.factory(),
+            backup_24px.factory(),
+            brightness_medium_24px.factory(),
+            help_24px.factory(),
+            info_24px.factory(),
+            keyboard_capslock_24px.factory(),
+            location_on_24px.factory(),
+            perm_device_information_24px.factory(),
+            storage_24px.factory(),
+            visibility_24px.factory(),
+            waves_24px.factory()
+        )
+        val currentAuroraSkin = mutableStateOf(twilightSkin())
+        val scope = rememberCoroutineScope()
+        val lazyListState = rememberLazyListState()
+        val stateSelection = mutableStateOf(-1)
+        val itemCount = 30
 
-    AuroraWindow(
-        title = "Aurora Scrollbars",
-        skin = currentAuroraSkin,
-        size = IntSize(250, 400),
-        undecorated = true,
-        keyboardShortcuts = mapOf(
-            Key.DirectionDown to {
-                if (stateSelection.value < 0) {
-                    stateSelection.value = 0
-                } else if (stateSelection.value < (itemCount - 1)) {
-                    stateSelection.value++
-                }
-            },
-            Key.DirectionUp to {
-                if (stateSelection.value < 0) {
-                    stateSelection.value = itemCount - 1
-                } else if (stateSelection.value > 0) {
-                    stateSelection.value--
-                }
-            })
-    ) {
-        Column(modifier = Modifier.fillMaxSize()) {
-            val currentSkinDisplayName = AuroraSkin.displayName
-            val auroraSkins = getAuroraSkins()
-            val selectedSkinItem =
-                remember { mutableStateOf(auroraSkins.first { it.first == currentSkinDisplayName }) }
-
-            ComboBoxProjection(
-                contentModel = ComboBoxContentModel(
-                    items = auroraSkins,
-                    selectedItem = selectedSkinItem.value,
-                    onTriggerItemSelectedChange = {
-                        selectedSkinItem.value = it
-                        currentAuroraSkin.value = it.second.invoke()
+        AuroraWindow(
+            title = "Aurora Scrollbars",
+            skin = currentAuroraSkin,
+            size = IntSize(250, 400),
+            undecorated = true,
+            keyboardShortcuts = mapOf(
+                Key.DirectionDown to {
+                    if (stateSelection.value < 0) {
+                        stateSelection.value = 0
+                    } else if (stateSelection.value < (itemCount - 1)) {
+                        stateSelection.value++
                     }
-                ),
-                presentationModel = ComboBoxPresentationModel(
-                    displayConverter = { it.first }
-                )
-            ).project()
+                    scope.launch {
+                        if (!lazyListState.isItemFullyVisible(stateSelection.value)) {
+                            lazyListState.scrollToItem(stateSelection.value)
+                        }
+                    }
+                },
+                Key.DirectionUp to {
+                    if (stateSelection.value < 0) {
+                        stateSelection.value = itemCount - 1
+                    } else if (stateSelection.value > 0) {
+                        stateSelection.value--
+                    }
+                    scope.launch {
+                        if (!lazyListState.isItemFullyVisible(stateSelection.value)) {
+                            lazyListState.scrollToItem(stateSelection.value)
+                        }
+                    }
+                })
+        ) {
+            Column(modifier = Modifier.fillMaxSize()) {
+                val currentSkinDisplayName = AuroraSkin.displayName
+                val auroraSkins = getAuroraSkins()
+                val selectedSkinItem =
+                    remember { mutableStateOf(auroraSkins.first { it.first == currentSkinDisplayName }) }
 
-            LabelProjection(
-                contentModel = LabelContentModel(
-                    text = if (stateSelection.value < 0) "No selection"
-                    else "Selected item #${stateSelection.value}"
-                ),
-                presentationModel = LabelPresentationModel(
-                    contentPadding = PaddingValues(
-                        start = 20.dp,
-                        end = 20.dp,
-                        top = 12.dp,
-                        bottom = 2.dp
+                ComboBoxProjection(
+                    contentModel = ComboBoxContentModel(
+                        items = auroraSkins,
+                        selectedItem = selectedSkinItem.value,
+                        onTriggerItemSelectedChange = {
+                            selectedSkinItem.value = it
+                            currentAuroraSkin.value = it.second.invoke()
+                        }
                     ),
-                    textStyle = TextStyle(fontWeight = FontWeight.Bold)
-                )
-            ).project()
-
-            Box(modifier = Modifier.fillMaxSize().padding(6.dp)) {
-                val itemsList = (0..itemCount).toList()
-                val state = rememberLazyListState()
-                LazyColumn(
-                    Modifier.fillMaxSize()
-                        .padding(end = ScrollBarSizingConstants.DefaultScrollBarThickness),
-                    state
-                ) {
-                    items(itemsList) { item ->
-                        AuroraBoxWithHighlights(
-                            modifier = Modifier.fillMaxWidth().height(32.dp),
-                            selected = (stateSelection.value == item),
-                            onClick = { stateSelection.value = item },
-                            sides = Sides(straightSides = Side.values().toSet()),
-                            content = {
-                                LabelProjection(
-                                    contentModel = LabelContentModel(
-                                        text = "Item #$item",
-                                        iconFactory = icons[item % icons.size]
-                                    ),
-                                    presentationModel = LabelPresentationModel(
-                                        inheritStateFromParent = true,
-                                        iconEnabledFilterStrategy = IconFilterStrategy.ThemedFollowText,
-                                        horizontalGapScaleFactor = 2.0f
-                                    )
-                                ).project()
-                            }
-                        )
-                    }
-                }
-                AuroraVerticalScrollbar(
-                    modifier = Modifier.align(Alignment.CenterEnd)
-                        .fillMaxHeight(),
-                    adapter = rememberScrollbarAdapter(
-                        scrollState = state
+                    presentationModel = ComboBoxPresentationModel(
+                        displayConverter = { it.first }
                     )
-                )
+                ).project()
+
+                LabelProjection(
+                    contentModel = LabelContentModel(
+                        text = if (stateSelection.value < 0) "No selection"
+                        else "Selected item #${stateSelection.value}"
+                    ),
+                    presentationModel = LabelPresentationModel(
+                        contentPadding = PaddingValues(
+                            start = 20.dp,
+                            end = 20.dp,
+                            top = 12.dp,
+                            bottom = 2.dp
+                        ),
+                        textStyle = TextStyle(fontWeight = FontWeight.Bold)
+                    )
+                ).project()
+
+                Box(modifier = Modifier.fillMaxSize().padding(6.dp)) {
+                    val itemsList = (0 until itemCount).toList()
+                    LazyColumn(
+                        Modifier.fillMaxSize()
+                            .padding(end = ScrollBarSizingConstants.DefaultScrollBarThickness),
+                        lazyListState
+                    ) {
+                        items(itemsList) { item ->
+                            AuroraBoxWithHighlights(
+                                modifier = Modifier.fillMaxWidth().height(32.dp),
+                                selected = (stateSelection.value == item),
+                                onClick = { stateSelection.value = item },
+                                sides = Sides(straightSides = Side.values().toSet()),
+                                content = {
+                                    LabelProjection(
+                                        contentModel = LabelContentModel(
+                                            text = "Item #$item",
+                                            iconFactory = icons[item % icons.size]
+                                        ),
+                                        presentationModel = LabelPresentationModel(
+                                            inheritStateFromParent = true,
+                                            iconEnabledFilterStrategy = IconFilterStrategy.ThemedFollowText,
+                                            horizontalGapScaleFactor = 2.0f
+                                        )
+                                    ).project()
+                                }
+                            )
+                        }
+                    }
+                    AuroraVerticalScrollbar(
+                        modifier = Modifier.align(Alignment.CenterEnd)
+                            .fillMaxHeight(),
+                        adapter = rememberScrollbarAdapter(
+                            scrollState = lazyListState
+                        )
+                    )
+                }
             }
         }
     }
