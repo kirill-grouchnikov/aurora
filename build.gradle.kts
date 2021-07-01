d736c2e9785c7dd7571ed282d340daa22a442418
Kirill Grouchnikov <kirillg@google.com>
2021-07-01 15:42:23 -0400

    Upgrade to build 229

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,7 +9,7 @@ buildscript {
     }
 
     dependencies {
-        classpath("org.jetbrains.compose:compose-gradle-plugin:0.5.0-build228")
+        classpath("org.jetbrains.compose:compose-gradle-plugin:0.5.0-build229")
         classpath(kotlin("gradle-plugin", version = "1.5.10"))
         classpath("com.github.ben-manes:gradle-versions-plugin:0.39.0")
         classpath("com.vanniktech:gradle-maven-publish-plugin:0.16.0")
