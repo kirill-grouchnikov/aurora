16f4e53c24e8ae25de90a368e71c343f162cee1c
Kirill Grouchnikov <kirillg@google.com>
2021-12-22 11:46:37 -0500

    Small tweaks
    Gradle -> 7.3.3
    Explicit dependency of tools/screenshot on coroutines
    Simplify classpath definition of screenshot scripts

--- scripts/screenshot/doc-robot-skins.sh
+++ scripts/screenshot/doc-robot-skins.sh
@@ -18,9 +18,9 @@ alias JAVA="java"
 
 AURORA_VERSION=1.1-SNAPSHOT
 KOTLIN_VERSION=1.6.10
-KOTLIN_COROUTINES_VERSION=1.5.2
+KOTLIN_COROUTINES_VERSION=1.6.0
 COMPOSE_VERSION=1.0.1-rc2
-CLASSPATH=../../drop/$AURORA_VERSION/aurora-screenshot-desktop-$AURORA_VERSION.jar:../../demo/build/libs/aurora-demo-desktop.jar:../../drop/$AURORA_VERSION/aurora-screenshot-common-$AURORA_VERSION.jar:../../drop/$AURORA_VERSION/aurora-screenshot-theming-$AURORA_VERSION.jar:../../drop/$AURORA_VERSION/aurora-screenshot-component-$AURORA_VERSION.jar:../../drop/$AURORA_VERSION/aurora-screenshot-window-$AURORA_VERSION.jar:../build/libs/batik-all-1.14.jar:../build/libs/xml-apis-1.4.01.jar:../build/libs/xml-apis-ext-1.3.04.jar:../../build/libs/xmlgraphics-commons-2.6.jar:../../build/libs/kotlin-stdlib-$KOTLIN_VERSION.jar:../../build/libs/kotlinx-coroutines-core-jvm-$KOTLIN_COROUTINES_VERSION.jar:../../build/libs/ui-graphics-desktop-$COMPOSE_VERSION.jar:../../build/libs/*
+CLASSPATH=../../drop/$AURORA_VERSION/aurora-screenshot-desktop-$AURORA_VERSION.jar:../../demo/build/libs/aurora-demo-desktop.jar:../build/libs/:../../build/libs/*
 
 java -cp $CLASSPATH org.pushingpixels.aurora.tools.screenshot.theming.skins.AuroraSkinsDriverKt \
     ../../docs/images/theming/skins
