d382b2920207ac717ab12d301911c03c99fa9753
Kirill Grouchnikov <kirillg@google.com>
2021-12-22 11:30:16 -0500

    Scripts for taking screenshots
    Also updated the visuals of Mist-skinned toolbar buttons

--- demo/build.gradle.kts
+++ demo/build.gradle.kts
@@ -1,5 +1,4 @@
 import org.jetbrains.compose.compose
-import org.jetbrains.compose.desktop.application.dsl.TargetFormat
 
 plugins {
     kotlin("multiplatform") // kotlin("jvm") doesn't work well in IDEA/AndroidStudio (https://github.com/JetBrains/compose-jb/issues/22)
@@ -17,19 +16,6 @@ kotlin {
                 implementation(project(":theming"))
                 implementation(project(":window"))
             }
-        }
-    }
-}
-
-compose.desktop {
-    application {
-        mainClass = "org.pushingpixels.aurora.demo.AuroraCommandDemoKt"
-
-        nativeDistributions {
-            targetFormats(TargetFormat.Dmg, TargetFormat.Msi, TargetFormat.Deb)
-            packageName = "AuroraDemo"
-            packageVersion = "1.0.0"
-            modules("jdk.crypto.ec")
         }
     }
 }
---
+++ scripts/screenshot/doc-robot-skins.sh
@@ -0,0 +1,26 @@
+#
+#  Copyright (c) 2020-2021 Aurora, Kirill Grouchnikov. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+export JAVA_HOME=`/usr/libexec/java_home -v 11`
+alias JAVA="java"
+
+AURORA_VERSION=1.1-SNAPSHOT
+KOTLIN_VERSION=1.6.10
+KOTLIN_COROUTINES_VERSION=1.5.2
+COMPOSE_VERSION=1.0.1-rc2
+CLASSPATH=../../drop/$AURORA_VERSION/aurora-screenshot-desktop-$AURORA_VERSION.jar:../../demo/build/libs/aurora-demo-desktop.jar:../../drop/$AURORA_VERSION/aurora-screenshot-common-$AURORA_VERSION.jar:../../drop/$AURORA_VERSION/aurora-screenshot-theming-$AURORA_VERSION.jar:../../drop/$AURORA_VERSION/aurora-screenshot-component-$AURORA_VERSION.jar:../../drop/$AURORA_VERSION/aurora-screenshot-window-$AURORA_VERSION.jar:../build/libs/batik-all-1.14.jar:../build/libs/xml-apis-1.4.01.jar:../build/libs/xml-apis-ext-1.3.04.jar:../../build/libs/xmlgraphics-commons-2.6.jar:../../build/libs/kotlin-stdlib-$KOTLIN_VERSION.jar:../../build/libs/kotlinx-coroutines-core-jvm-$KOTLIN_COROUTINES_VERSION.jar:../../build/libs/ui-graphics-desktop-$COMPOSE_VERSION.jar:../../build/libs/*
+
+java -cp $CLASSPATH org.pushingpixels.aurora.tools.screenshot.theming.skins.AuroraSkinsDriverKt \
+    ../../docs/images/theming/skins
