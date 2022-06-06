f1ee968ed863bb221b525dcd65c6dfa6abf21d29
Kirill Grouchnikov <kirillg@google.com>
2022-06-06 12:45:00 -0400

    Upgrade dependencies
    Coroutines -> 1.6.2
    Publish plugin -> 0.20.0

--- scripts/screenshot/doc-robot-skins.sh
+++ scripts/screenshot/doc-robot-skins.sh
@@ -18,7 +18,7 @@ alias JAVA="java"
 
 AURORA_VERSION=1.2-SNAPSHOT
 KOTLIN_VERSION=1.6.21
-KOTLIN_COROUTINES_VERSION=1.6.1
+KOTLIN_COROUTINES_VERSION=1.6.2
 COMPOSE_VERSION=1.2.0-alpha01-dev675
 CLASSPATH=../../drop/$AURORA_VERSION/aurora-screenshot-desktop-$AURORA_VERSION.jar:../../demo/build/libs/aurora-demo-desktop.jar:../build/libs/:../../build/libs/*
 
