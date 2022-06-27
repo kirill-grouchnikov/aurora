c8a445aefb7a9c534d0eb2c2aa0ea7db20e16a8d
Kirill Grouchnikov <kirill.grouchnikov@gmail.com>
2022-06-27 13:23:41 -0400

    Upgrade dependencies
    Coroutines -> 1.6.3

--- tools/screenshot/build.gradle.kts
+++ tools/screenshot/build.gradle.kts
@@ -14,7 +14,7 @@ kotlin {
         named("desktopMain") {
             dependencies {
                 api(compose.desktop.currentOs)
-                implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.2")
+                implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.3")
                 implementation(project(":common"))
                 implementation(project(":component"))
                 implementation(project(":theming"))
