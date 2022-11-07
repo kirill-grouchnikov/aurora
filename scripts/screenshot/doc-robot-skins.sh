273ac9fffb9187d253be9a0a17e2eda30fa700d2
Kirill Grouchnikov <kirill.grouchnikov@gmail.com>
2022-11-07 09:22:58 -0500

    Upgrade to CfD 1.2.1

--- scripts/screenshot/doc-robot-skins.sh
+++ scripts/screenshot/doc-robot-skins.sh
@@ -19,7 +19,7 @@ alias JAVA="java"
 AURORA_VERSION=1.3-SNAPSHOT
 KOTLIN_VERSION=1.7.20
 KOTLIN_COROUTINES_VERSION=1.6.4
-COMPOSE_VERSION=1.2.0
+COMPOSE_VERSION=1.2.1
 CLASSPATH=../../drop/$AURORA_VERSION/aurora-screenshot-desktop-$AURORA_VERSION.jar:../../demo/build/libs/aurora-demo-desktop.jar:../build/libs/:../../build/libs/*
 
 java -cp $CLASSPATH org.pushingpixels.aurora.tools.screenshot.theming.skins.AuroraSkinsDriverKt \
