dea19a210bfb31493cc4df5201049d3c4b651210
Kirill Grouchnikov <kirill.grouchnikov@gmail.com>
2023-01-01 16:52:05 -0500

    Update license header to 2023

--- component/src/desktopMain/kotlin/org/pushingpixels/aurora/component/model/CommandPopupMenuPresentationModel.kt
+++ component/src/desktopMain/kotlin/org/pushingpixels/aurora/component/model/CommandPopupMenuPresentationModel.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright 2020-2022 Aurora, Kirill Grouchnikov
+ * Copyright 2020-2023 Aurora, Kirill Grouchnikov
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
