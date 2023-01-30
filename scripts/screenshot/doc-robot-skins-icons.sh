d2bcc36a393751304ba34d1b3c90b9920cbed537
Kirill Grouchnikov <kirill.grouchnikov@gmail.com>
2023-01-30 11:54:40 -0500

    Upgrade Compose
    Compose -> 1.3.0
    Kotlin -> 1.8.0

--- scripts/screenshot/doc-robot-skins-icons.sh
+++ scripts/screenshot/doc-robot-skins-icons.sh
@@ -17,9 +17,9 @@ export JAVA_HOME=`/usr/libexec/java_home -v 11`
 alias JAVA="java"
 
 AURORA_VERSION=1.4-SNAPSHOT
-KOTLIN_VERSION=1.7.20
+KOTLIN_VERSION=1.8.0
 KOTLIN_COROUTINES_VERSION=1.6.4
-COMPOSE_VERSION=1.2.2
+COMPOSE_VERSION=1.3.0
 CLASSPATH=../../drop/$AURORA_VERSION/aurora-screenshot-desktop-$AURORA_VERSION.jar:../../demo/build/libs/aurora-demo-desktop.jar:../build/libs/:../../build/libs/*
 
 java -cp $CLASSPATH org.pushingpixels.aurora.tools.screenshot.theming.skins.AuroraSkinsIconFilterDriverKt \
