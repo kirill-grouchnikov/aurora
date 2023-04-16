cb391522409a501a4c6c8987d39bec81b83aaa3e
Kirill Grouchnikov <kirill.grouchnikov@gmail.com>
2023-04-16 14:38:06 -0400

    Update event handling to not use the deprecated method
    Take the latest changes from https://github.com/JetBrains/compose-multiplatform-core/blob/jb-main/compose/foundation/foundation/src/desktopMain/kotlin/androidx/compose/foundation/Scrollbar.desktop.kt and https://github.com/JetBrains/compose-multiplatform-core/blob/jb-main/compose/foundation/foundation/src/commonMain/kotlin/androidx/compose/foundation/gestures/TapGestureDetector.kt

--- component/src/desktopMain/kotlin/org/pushingpixels/aurora/component/AuroraScrollBar.kt
+++ component/src/desktopMain/kotlin/org/pushingpixels/aurora/component/AuroraScrollBar.kt
@@ -37,10 +37,7 @@ import androidx.compose.ui.layout.MeasurePolicy
 import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.platform.LocalLayoutDirection
 import androidx.compose.ui.unit.*
-import kotlinx.coroutines.coroutineScope
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.*
 import kotlinx.coroutines.sync.Mutex
 import org.pushingpixels.aurora.common.AuroraInternalApi
 import org.pushingpixels.aurora.component.utils.ModelStateInfo
@@ -417,8 +414,7 @@ private fun Scrollbar(
                 }
             }
         },
-        modifier
-            .scrollOnPressOutsideSlider(isVertical, sliderAdapter, adapter, containerSize),
+        modifier.scrollOnPressTrack(isVertical, sliderAdapter),
         measurePolicy
     )
 }
@@ -431,70 +427,196 @@ private fun Modifier.scrollbarDrag(
     val currentInteractionSource by rememberUpdatedState(interactionSource)
     val currentDraggedInteraction by rememberUpdatedState(draggedInteraction)
     val currentSliderAdapter by rememberUpdatedState(sliderAdapter)
+
     pointerInput(Unit) {
-        forEachGesture {
-            awaitPointerEventScope {
-                val down = awaitFirstDown(requireUnconsumed = false)
-                val interaction = DragInteraction.Start()
-                currentInteractionSource.tryEmit(interaction)
-                currentDraggedInteraction.value = interaction
-                currentSliderAdapter.onDragStarted()
-                val isSuccess = drag(down.id) { change ->
-                    currentSliderAdapter.onDragDelta(change.positionChange())
-                    change.consume()
-                }
-                val finishInteraction = if (isSuccess) {
-                    DragInteraction.Stop(interaction)
-                } else {
-                    DragInteraction.Cancel(interaction)
-                }
-                currentInteractionSource.tryEmit(finishInteraction)
-                currentDraggedInteraction.value = null
+        awaitEachGesture {
+            val down = awaitFirstDown(requireUnconsumed = false)
+            val interaction = DragInteraction.Start()
+            currentInteractionSource.tryEmit(interaction)
+            currentDraggedInteraction.value = interaction
+            currentSliderAdapter.onDragStarted()
+            val isSuccess = drag(down.id) { change ->
+                currentSliderAdapter.onDragDelta(change.positionChange())
+                change.consume()
             }
+            val finishInteraction = if (isSuccess) {
+                DragInteraction.Stop(interaction)
+            } else {
+                DragInteraction.Cancel(interaction)
+            }
+            currentInteractionSource.tryEmit(finishInteraction)
+            currentDraggedInteraction.value = null
         }
     }
 }
 
-private fun Modifier.scrollOnPressOutsideSlider(
+private fun Modifier.scrollOnPressTrack(
     isVertical: Boolean,
     sliderAdapter: SliderAdapter,
-    scrollbarAdapter: ScrollbarAdapter,
-    containerSize: Int
 ) = composed {
-    var targetOffset: Offset? by remember { mutableStateOf(null) }
-
-    if (targetOffset != null) {
-        val targetPosition = if (isVertical) targetOffset!!.y else targetOffset!!.x
-
-        LaunchedEffect(targetPosition) {
-            var delay = PressTimeoutMillis * 3
-            while (targetPosition !in sliderAdapter.bounds) {
-                val oldSign = sign(targetPosition - sliderAdapter.position)
-                scrollbarAdapter.scrollTo(
-                    scrollbarAdapter.scrollOffset + oldSign * containerSize
-                )
-                val newSign = sign(targetPosition - sliderAdapter.position)
-
-                if (oldSign != newSign) {
-                    break
-                }
-
-                delay(delay)
-                delay = PressTimeoutMillis
-            }
-        }
+    val coroutineScope = rememberCoroutineScope()
+    val scroller = remember(sliderAdapter, coroutineScope) {
+        TrackPressScroller(coroutineScope, sliderAdapter)
     }
-    Modifier.pointerInput(Unit) {
-        detectTapAndPress(
-            onPress = { offset ->
-                targetOffset = offset
-                tryAwaitRelease()
-                targetOffset = null
-            },
-            onTap = {}
+    Modifier.pointerInput(scroller) {
+        detectScrollViaTrackGestures(
+            isVertical = isVertical,
+            scroller = scroller
         )
     }
 }
+
+/**
+ * Responsible for scrolling when the scrollbar track is pressed (outside the thumb).
+ */
+private class TrackPressScroller(
+    private val coroutineScope: CoroutineScope,
+    private val sliderAdapter: SliderAdapter
+) {
+
+    /**
+     * The current direction of scroll (1: down/right, -1: up/left, 0: not scrolling)
+     */
+    private var direction = 0
+
+    /**
+     * The currently pressed location (in pixels) on the scrollable axis.
+     */
+    private var offset: Float? = null
+
+    /**
+     * The job that keeps scrolling while the track is pressed.
+     */
+    private var job: Job? = null
+
+    /**
+     * Calculates the direction of scrolling towards the given offset (in pixels).
+     */
+    private fun directionOfScrollTowards(offset: Float): Int {
+        val thumbRange = sliderAdapter.bounds
+        return when {
+            offset < thumbRange.start -> -1
+            offset > thumbRange.endInclusive -> 1
+            else -> 0
+        }
+    }
+
+    /**
+     * Scrolls once towards the current offset, if it matches the direction of the current gesture.
+     */
+    private suspend fun scrollTowardsCurrentOffset() {
+        offset?.let {
+            val currentDirection = directionOfScrollTowards(it)
+            if (currentDirection != direction)
+                return
+            with(sliderAdapter.adapter) {
+                scrollTo(scrollOffset + currentDirection * viewportSize)
+            }
+        }
+    }
+
+    /**
+     * Starts the job that scrolls continuously towards the current offset.
+     */
+    private fun startScrolling() {
+        job?.cancel()
+        job = coroutineScope.launch {
+            scrollTowardsCurrentOffset()
+            delay(DelayBeforeSecondScrollOnTrackPress)
+            while (true) {
+                scrollTowardsCurrentOffset()
+                delay(DelayBetweenScrollsOnTrackPress)
+            }
+        }
+    }
+
+    /**
+     * Invoked on the first press for a gesture.
+     */
+    fun onPress(offset: Float) {
+        this.offset = offset
+        this.direction = directionOfScrollTowards(offset)
+
+        if (direction != 0)
+            startScrolling()
+    }
+
+    /**
+     * Invoked when the pointer moves while pressed during the gesture.
+     */
+    fun onMovePressed(offset: Float) {
+        this.offset = offset
+    }
+
+    /**
+     * Cleans up when the gesture finishes.
+     */
+    private fun cleanupAfterGesture(){
+        job?.cancel()
+        direction = 0
+        offset = null
+    }
+
+    /**
+     * Invoked when the button is released.
+     */
+    fun onRelease() {
+        cleanupAfterGesture()
+    }
+
+    /**
+     * Invoked when the gesture is cancelled.
+     */
+    fun onGestureCancelled() {
+        cleanupAfterGesture()
+        // Maybe revert to the initial position?
+    }
+
+}
+
+/**
+ * Detects the pointer events relevant for the "scroll by pressing on the track outside the thumb"
+ * gesture and calls the corresponding methods in the [scroller].
+ */
+private suspend fun PointerInputScope.detectScrollViaTrackGestures(
+    isVertical: Boolean,
+    scroller: TrackPressScroller
+) {
+    fun Offset.onScrollAxis() = if (isVertical) y else x
+
+    awaitEachGesture {
+        val down = awaitFirstDown()
+        scroller.onPress(down.position.onScrollAxis())
+
+        while (true) {
+            val drag =
+                if (isVertical)
+                    awaitVerticalDragOrCancellation(down.id)
+                else
+                    awaitHorizontalDragOrCancellation(down.id)
+
+            if (drag == null) {
+                scroller.onGestureCancelled()
+                break
+            } else if (!drag.pressed) {
+                scroller.onRelease()
+                break
+            } else
+                scroller.onMovePressed(drag.position.onScrollAxis())
+        }
+    }
+}
+
+/**
+ * The delay between the 1st and 2nd scroll while the scrollbar track is pressed outside the thumb.
+ */
+internal const val DelayBeforeSecondScrollOnTrackPress: Long = 300L
+
+/**
+ * The delay between each subsequent (after the 2nd) scroll while the scrollbar track is pressed
+ * outside the thumb.
+ */
+internal const val DelayBetweenScrollsOnTrackPress: Long = 100L
 
 /**
  * The maximum scroll offset of the scrollable content.
@@ -632,25 +754,31 @@ internal suspend fun PointerInputScope.detectTapAndPress(
     onTap: ((Offset) -> Unit)? = null
 ) {
     val pressScope = PressGestureScopeImpl(this)
-    forEachGesture {
-        coroutineScope {
-            pressScope.reset()
-            awaitPointerEventScope {
+    coroutineScope {
+        awaitEachGesture {
+            launch {
+                pressScope.reset()
+            }
 
-                val down = awaitFirstDown().also { it.consume() }
+            val down = awaitFirstDown().also { it.consume() }
 
-                if (onPress !== NoPressGesture) {
-                    launch { pressScope.onPress(down.position) }
+            if (onPress !== NoPressGesture) {
+                launch {
+                    pressScope.onPress(down.position)
                 }
+            }
 
-                val up = waitForUpOrCancellation()
-                if (up == null) {
+            val up = waitForUpOrCancellation()
+            if (up == null) {
+                launch {
                     pressScope.cancel() // tap-up was canceled
-                } else {
-                    up.consume()
-                    pressScope.release()
-                    onTap?.invoke(up.position)
                 }
+            } else {
+                up.consume()
+                launch {
+                    pressScope.release()
+                }
+                onTap?.invoke(up.position)
             }
         }
     }
