49161e1974959f30411587a237f703f275894d0f
Kirill Grouchnikov <kirill.grouchnikov@gmail.com>
2023-08-11 16:09:37 -0400

    Upgrade dependencies
    Compose -> 1.5.0-beta02
    Dokka -> 1.9.0-dev-228

--- scripts/screenshot/doc-robot-skins.sh
+++ scripts/screenshot/doc-robot-skins.sh
@@ -19,7 +19,7 @@ alias JAVA="java"
 AURORA_VERSION=2.0-SNAPSHOT
 KOTLIN_VERSION=1.9.0
 KOTLIN_COROUTINES_VERSION=1.6.4
-COMPOSE_VERSION=1.5.0-beta01
+COMPOSE_VERSION=1.5.0-beta02
 CLASSPATH=../../drop/$AURORA_VERSION/aurora-screenshot-desktop-$AURORA_VERSION.jar:../../demo/build/libs/aurora-demo-desktop.jar:../build/libs/:../../build/libs/*
 
 java -cp $CLASSPATH org.pushingpixels.aurora.tools.screenshot.theming.skins.AuroraSkinsDriverKt \
