1a3b2cc06e36713718a5a41a10e3318c314efcdd
Kirill Grouchnikov <kirill.grouchnikov@gmail.com>
2024-04-12 17:01:15 -0400

    Upgrade dependencies
    Kotlin -> 2.0.0-RC1

--- scripts/screenshot/doc-robot-schemes.sh
+++ scripts/screenshot/doc-robot-schemes.sh
@@ -17,7 +17,7 @@ export JAVA_HOME=`/usr/libexec/java_home -v 11`
 alias JAVA="java"
 
 AURORA_VERSION=2.0-SNAPSHOT
-KOTLIN_VERSION=1.9.23
+KOTLIN_VERSION=2.0.0-RC1
 KOTLIN_COROUTINES_VERSION=1.6.4
 COMPOSE_VERSION=1.6.2
 CLASSPATH=../../drop/$AURORA_VERSION/aurora-screenshot-desktop-$AURORA_VERSION.jar:../../demo/build/libs/aurora-demo-desktop.jar:../build/libs/:../../build/libs/*
