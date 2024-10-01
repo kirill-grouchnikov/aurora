abae35f148f9ff9baa4d2c5d446a39794e2c72f3
Kirill Grouchnikov <kirill.grouchnikov@gmail.com>
2024-10-01 09:39:10 -0400

    Upgrade dependencies
    Compose -> 1.7.0-rc01

--- scripts/screenshot/doc-robot-schemes.sh
+++ scripts/screenshot/doc-robot-schemes.sh
@@ -19,7 +19,7 @@ alias JAVA="java"
 AURORA_VERSION=2.0-SNAPSHOT
 KOTLIN_VERSION=2.0.0
 KOTLIN_COROUTINES_VERSION=1.6.4
-COMPOSE_VERSION=1.7.0-beta02
+COMPOSE_VERSION=1.7.0-rc01
 CLASSPATH=../../drop/$AURORA_VERSION/aurora-screenshot-desktop-$AURORA_VERSION.jar:../../demo/build/libs/aurora-demo-desktop.jar:../build/libs/:../../build/libs/*
 
 java -cp $CLASSPATH org.pushingpixels.aurora.tools.screenshot.theming.schemes.AuroraColorSchemesDriverKt \
