e604ed0a8629244efabdfde0e906d5000260ed75
Kirill Grouchnikov <kirill.grouchnikov@gmail.com>
2025-11-07 16:34:20 -0500

    Add the first batch of tonal walkthrough demos

---
+++ demo/src/desktopMain/kotlin/org/pushingpixels/aurora/demo/tonal/TonalPalettePreview.kt
@@ -0,0 +1,73 @@
+/*
+ * Copyright 2020-2025 Aurora, Kirill Grouchnikov
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.pushingpixels.aurora.demo.tonal
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.*
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.unit.TextUnit
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import org.pushingpixels.aurora.component.model.HorizontalAlignment
+import org.pushingpixels.aurora.component.model.LabelContentModel
+import org.pushingpixels.aurora.component.model.LabelPresentationModel
+import org.pushingpixels.aurora.component.projection.LabelProjection
+import org.pushingpixels.aurora.theming.resolveAuroraDefaults
+import org.pushingpixels.ephemeral.chroma.palettes.BaseTonalPalette
+
+@Composable
+fun TonalPalettePreview(tonalPalette: BaseTonalPalette, showLegend: Boolean = true) {
+    val textStyle = resolveAuroraDefaults()
+    val fontSize = textStyle.fontSize
+    // Compute a smaller font size
+    val smallerFontSize = TextUnit(fontSize.value - 1.5f, fontSize.type)
+
+    Column(modifier = Modifier.width(560.dp).padding(horizontal = 10.dp)) {
+        if (showLegend) {
+            Row(modifier = Modifier.fillMaxWidth(1.0f)) {
+                for (tone in 0..100 step 5) {
+                    Box(modifier = Modifier.size(24.dp), contentAlignment = Alignment.Center) {
+                        LabelProjection(
+                            contentModel = LabelContentModel(text = tone.toString()),
+                            presentationModel = LabelPresentationModel(
+                                contentPadding = PaddingValues(0.dp),
+                                horizontalAlignment = HorizontalAlignment.Center,
+                                textStyle = textStyle.copy(fontSize = smallerFontSize, letterSpacing = (-0.4).sp),
+                                textMaxLines = 1,
+                                textOverflow = TextOverflow.Visible
+                            )
+                        ).project(modifier = Modifier.width(24.dp))
+                    }
+                }
+            }
+        }
+        Row(modifier = Modifier.fillMaxWidth(1.0f)) {
+            for (tone in 0..100 step 5) {
+                val toneRgb = tonalPalette.tone(tone)
+                Box(
+                    modifier = Modifier.size(24.dp)
+                        .border(width = 0.dp, color = Color.Black)
+                        .background(color = Color(toneRgb))
+                )
+            }
+        }
+    }
+}
---
+++ demo/src/desktopMain/kotlin/org/pushingpixels/aurora/demo/tonal/walkthrough/Step2ChromaDemo.kt
@@ -0,0 +1,84 @@
+/*
+ * Copyright 2020-2025 Aurora, Kirill Grouchnikov
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.pushingpixels.aurora.demo.tonal.walkthrough;
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.DpSize
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.window.WindowPlacement
+import androidx.compose.ui.window.WindowPosition
+import androidx.compose.ui.window.rememberWindowState
+import org.pushingpixels.aurora.component.model.HorizontalAlignment
+import org.pushingpixels.aurora.component.model.LabelContentModel
+import org.pushingpixels.aurora.component.model.LabelPresentationModel
+import org.pushingpixels.aurora.component.projection.LabelProjection
+import org.pushingpixels.aurora.demo.svg.radiance_menu
+import org.pushingpixels.aurora.demo.tonal.TonalPalettePreview
+import org.pushingpixels.aurora.theming.IconFilterStrategy
+import org.pushingpixels.aurora.theming.businessSkin
+import org.pushingpixels.aurora.window.AuroraWindow
+import org.pushingpixels.aurora.window.AuroraWindowTitlePaneConfigurations
+import org.pushingpixels.aurora.window.auroraApplication
+import org.pushingpixels.ephemeral.chroma.hct.Hct
+import org.pushingpixels.ephemeral.chroma.palettes.TonalPalette
+
+fun main() = auroraApplication {
+    val state = rememberWindowState(
+            placement = WindowPlacement.Floating,
+            position = WindowPosition.Aligned(Alignment.Center),
+            size = DpSize(670.dp, 680.dp)
+    )
+    AuroraWindow(
+            skin = businessSkin(),
+            title = "Chroma Palette",
+            state = state,
+            windowTitlePaneConfiguration = AuroraWindowTitlePaneConfigurations.AuroraPlain(),
+            icon = radiance_menu(),
+            iconFilterStrategy = IconFilterStrategy.ThemedFollowText,
+            onCloseRequest = ::exitApplication,
+    ) {
+        Column(modifier = Modifier.fillMaxSize().padding(top = 12.dp)) {
+            for (i in 0..20) {
+                Row(modifier = Modifier.fillMaxWidth(), verticalAlignment = Alignment.Bottom) {
+                    val chroma = i * 4.0
+                    LabelProjection(
+                        contentModel = LabelContentModel(text = "Chroma=$chroma"),
+                        presentationModel = LabelPresentationModel(
+                            contentPadding = PaddingValues(0.dp),
+                            horizontalAlignment = HorizontalAlignment.Trailing,
+                            textMaxLines = 1,
+                            singleLineDisplayPrototype = "Chroma=80.0"
+                        )
+                    ).project(modifier = Modifier.padding(start = 20.dp, end = 10.dp, bottom = 4.dp))
+                    TonalPalettePreview(
+                        tonalPalette = TonalPalette.fromHct(Hct.from(340.0, chroma, 40.0)),
+                        showLegend = (i == 0)
+                    )
+                }
+                Spacer(modifier = Modifier.height(4.dp))
+            }
+        }
+    }
+}
