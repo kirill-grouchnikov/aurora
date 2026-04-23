74cad8a4f1542f078375756a5dec3a2cc09feb1f
Kirill Grouchnikov <kirill.grouchnikov@gmail.com>
2026-04-23 15:11:51 -0400

    Tweak the component shaper demo skin

--- demo/src/desktopMain/kotlin/org/pushingpixels/aurora/demo/GeminiSkinWithRectangularOutlines.kt
+++ demo/src/desktopMain/kotlin/org/pushingpixels/aurora/demo/GeminiSkinWithRectangularOutlines.kt
@@ -16,6 +16,7 @@
 package org.pushingpixels.aurora.demo
 
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.ui.geometry.CornerRadius
 import androidx.compose.ui.geometry.Rect
 import androidx.compose.ui.geometry.RoundRect
 import androidx.compose.ui.geometry.Size
@@ -33,11 +34,83 @@ fun geminiSkinWithRectangularOutlines(): AuroraSkinDefinition {
 }
 
 private class RectangularComponentShaper: AuroraComponentShaper {
+    private fun getRectangularOutline(
+        layoutDirection: LayoutDirection,
+        width: Float, height: Float,
+        sides: Sides? = null,
+        insets: Float = 0.0f, outlineKind: OutlineKind = OutlineKind.Outline
+    ): Outline {
+        val leftSide = if (layoutDirection == LayoutDirection.Ltr) Side.Leading else Side.Trailing
+        val rightSide = if (layoutDirection == LayoutDirection.Ltr) Side.Trailing else Side.Leading
+
+        val openSides = sides?.openSides
+        val hasOpenSides = !openSides.isNullOrEmpty()
+
+        if (!hasOpenSides) {
+            // No open sides? A simple rectangle
+            return Outline.Rectangle(
+                rect = Rect(
+                    left = insets, top = insets,
+                    right = width - insets, bottom = height - insets
+                )
+            )
+        }
+        // Open sides are handled differently for fill and border outline kinds. For fill we need
+        // to return a fully continuous and closed path so that it can be filled. For border, we
+        // respect the open sides, using moveTo instead of lineTo for the relevant side(s).
+        val isTopOpen = openSides.contains(Side.Top) && (outlineKind == OutlineKind.Outline)
+        val isBottomOpen = openSides.contains(Side.Bottom) && (outlineKind == OutlineKind.Outline)
+        val isLeftOpen = openSides.contains(leftSide) && (outlineKind == OutlineKind.Outline)
+        val isRightOpen = openSides.contains(rightSide) && (outlineKind == OutlineKind.Outline)
+
+        val path = Path()
+        // Start in top left
+        path.moveTo(x = insets, y = insets)
+        // To top right
+        if (isTopOpen) {
+            path.moveTo(x = width - insets, y = insets)
+        } else {
+            path.lineTo(x = width - insets, y = insets)
+        }
+        // To bottom right
+        if (isRightOpen) {
+            path.moveTo(x = width - insets, y = height - insets)
+        } else {
+            path.lineTo(x = width - insets, y = height - insets)
+        }
+
+        // To bottom left
+        if (isBottomOpen) {
+            path.moveTo(x = insets, y = height - insets)
+        } else {
+            path.lineTo(x = insets, y = height - insets)
+        }
+        // To top left
+        if (isLeftOpen) {
+            path.moveTo(x = insets, y = insets)
+        } else {
+            path.lineTo(x = insets, y = insets)
+        }
+
+        return Outline.Generic(path = path)
+    }
+
     override val displayName: String
         get() = "Rectangular"
 
     override fun getButtonOutlineSupplier(buttonSides: Sides): OutlineSupplier {
-        return RectangleOutlineSuppler
+        return object: OutlineSupplier {
+            override fun getOutline(
+                layoutDirection: LayoutDirection,
+                density: Density,
+                size: Size,
+                insets: Float,
+                radiusAdjustment: Float,
+                outlineKind: OutlineKind
+            ): Outline {
+                return getRectangularOutline(layoutDirection, size.width, size.height, buttonSides, insets, outlineKind)
+            }
+        }
     }
 
     override fun getButtonExtraContentPadding(
@@ -93,11 +166,22 @@ private class RectangularComponentShaper: AuroraComponentShaper {
     }
 
     override fun getSwitchTrackOutlineSupplier(): OutlineSupplier {
-        return RectangleOutlineSuppler
+        return SwitchTrackOutlineSuppler
     }
 
     override fun getTabButtonOutlineSupplier(sides: Sides): OutlineSupplier {
-        return RectangleOutlineSuppler
+        return object: OutlineSupplier {
+            override fun getOutline(
+                layoutDirection: LayoutDirection,
+                density: Density,
+                size: Size,
+                insets: Float,
+                radiusAdjustment: Float,
+                outlineKind: OutlineKind
+            ): Outline {
+                return getRectangularOutline(layoutDirection, size.width, size.height, sides, insets, outlineKind)
+            }
+        }
     }
 
     override fun getTextFieldOutlineSupplier(): OutlineSupplier {
@@ -168,6 +252,26 @@ private class RectangularComponentShaper: AuroraComponentShaper {
                 path.close()
 
                 return Outline.Generic(path)
+            }
+        }
+
+        private val SwitchTrackOutlineSuppler = object: OutlineSupplier {
+            override fun getOutline(
+                layoutDirection: LayoutDirection,
+                density: Density,
+                size: Size,
+                insets: Float,
+                radiusAdjustment: Float,
+                outlineKind: OutlineKind
+            ): Outline {
+                val radius = size.height / 2.0f - radiusAdjustment
+                return Outline.Rounded(
+                    roundRect = RoundRect(
+                        left = insets, top = insets,
+                        right = size.width - insets, bottom = size.height - insets,
+                        cornerRadius = CornerRadius(radius, radius)
+                    )
+                )
             }
         }
     }
