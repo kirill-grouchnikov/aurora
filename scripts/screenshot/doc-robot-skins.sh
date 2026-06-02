47db2e2e477334c4b60d06c6e392685dd4bb2bbb
Kirill Grouchnikov <kirill.grouchnikov@gmail.com>
2026-06-02 12:14:49 -0400

    Upgrade dependencies
    Compose -> 1.11.1
    Gradle -> 9.5.1

--- scripts/screenshot/doc-robot-skins.sh
+++ scripts/screenshot/doc-robot-skins.sh
@@ -19,7 +19,7 @@ alias JAVA="java"
 AURORA_VERSION=2.5-SNAPSHOT
 KOTLIN_VERSION=2.3.20
 KOTLIN_COROUTINES_VERSION=1.11.0
-COMPOSE_VERSION=1.11.0
+COMPOSE_VERSION=1.11.1
 CLASSPATH=../../drop/$AURORA_VERSION/aurora-screenshot-desktop-$AURORA_VERSION.jar:../../demo/build/libs/aurora-demo-desktop.jar:../../build/libs/*
 
 java -cp $CLASSPATH org.pushingpixels.aurora.tools.screenshot.theming.skins.AuroraSkinsDriverKt \
