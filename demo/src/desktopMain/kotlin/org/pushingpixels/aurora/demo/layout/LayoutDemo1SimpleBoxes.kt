fde08bfffda160ce00fe7224208c67cc25a45d80
Kirill Grouchnikov <kirill.grouchnikov@gmail.com>
2026-08-27 16:54:08 -0400

    Rename layout demos
    For #99

--- demo/src/desktopMain/kotlin/org/pushingpixels/aurora/demo/layout/LayoutDemo1SimpleBoxes.kt
+++ demo/src/desktopMain/kotlin/org/pushingpixels/aurora/demo/layout/LayoutDemo1SimpleBoxes.kt
@@ -19,7 +19,6 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
